a90640bf7de3e40d304069a1b977313eb97aff85
卡欧司 <1140234993@qq.com>
2022-06-10 10:30:38 +0800

    真完成结构搭建，可以跑了

---
+++ src/main/java/com/proud/controller/HelloWord.java
@@ -0,0 +1,13 @@
+package com.proud.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWord {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "HelloWord";
+    }
+}
