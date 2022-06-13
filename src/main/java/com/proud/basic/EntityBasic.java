14fc005127a316251ef2d4b5acd6d97d22e3297f
刘龙 <kercylan@gmail.com>
2022-06-13 17:44:20 +0800

    数据库表自动生成

--- src/main/java/com/proud/basic/EntityBasic.java
+++ src/main/java/com/proud/basic/EntityBasic.java
@@ -1,11 +1,17 @@
 package com.proud.basic;
 
+import com.alibaba.druid.sql.visitor.functions.Now;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
 import com.gitee.sunchenbin.mybatis.actable.annotation.IsAutoIncrement;
 import com.gitee.sunchenbin.mybatis.actable.annotation.IsKey;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public abstract class EntityBasic {
@@ -15,4 +21,16 @@ public abstract class EntityBasic {
     @TableId(type = IdType.AUTO)
     @Column(comment = "ID")
     private Long id;
+
+    @ColumnType(MySqlTypeConstant.DATETIME)
+    @Column(comment = "创建时间", isNull = false, defaultValue = "now()")
+    private Date createAt;
+
+    @ColumnType(MySqlTypeConstant.DATETIME)
+    @Column(comment = "删除时间", isNull = true, defaultValue = "NULL")
+    private Date deleteAt;
+
+    @ColumnType(MySqlTypeConstant.DATETIME)
+    @Column(comment = "更新时间", isNull = false, defaultValue = "now()")
+    private Date updateAt;
 }
